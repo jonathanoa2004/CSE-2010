5a92bfac90ac152110919cfa3fe8be1d2bba0ddf
jonathanoa2004 <158521796+jonathanoa2004@users.noreply.github.com>
2024-02-13 21:27:18 -0800

    Add files via upload

--- Lab2_num_obj.cpp
+++ Lab2_num_obj.cpp
@@ -1,1 +1,12 @@
 //Lab2_num_obj.cpp
+
+#include <iostream>
+#include <cstdlib>
+
+int main() {
+    // numerical
+    int int_example = 1;
+    float float_example = 2.0;
+    double double_example = 1.0;
+
+
