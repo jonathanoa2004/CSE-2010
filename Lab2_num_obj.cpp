541266a33c52edf82ff82a1a04e08affbf4f49ae
jonathanoa2004 <158521796+jonathanoa2004@users.noreply.github.com>
2024-02-13 21:30:34 -0800

    added float examples

--- Lab2_num_obj.cpp
+++ Lab2_num_obj.cpp
@@ -9,4 +9,14 @@ int main() {
     float float_example = 2.0;
     double double_example = 1.0;
 
+    // non-numerical
+    char char_example = 'a';
+    bool bool_example = true;
+
+  // sizeof returns the number of bytes (8 bits is equivalent to 1 byte))
+    cout << "int_example: " << sizeof(int_example) << endl;
+    cout << "float_example: " << sizeof(float_example) << endl;
+    cout << "double_example: " << sizeof(double_example) << endl;
+    cout << "char_example: " << sizeof(char_example) << endl;
+    cout << "bool_example: " << sizeof(bool_example) << endl;
 
