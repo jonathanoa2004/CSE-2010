daff1bfd8e373c0fbebae6d602529aed425c9245
jonathanoa2004 <158521796+jonathanoa2004@users.noreply.github.com>
2024-02-13 21:40:39 -0800

    Update Lab2_calc.cpp

--- Lab 2/Lab2_calc.cpp
+++ Lab 2/Lab2_calc.cpp
@@ -10,3 +10,7 @@ int main() {
     int int_x = 1;
     int int_y = 2;
     double double_z = 2.0;
+
+    cout << "int divided by int: " << int_x / int_y << endl;
+    cout << "int divided by double: " << int_x / double_z << endl;
+
