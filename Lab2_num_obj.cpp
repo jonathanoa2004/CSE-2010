83ba03e67de3795ec179fb955a45f4a86de0a7e3
jonathanoa2004 <158521796+jonathanoa2004@users.noreply.github.com>
2024-02-13 21:32:29 -0800

    Add files via upload

--- Lab2_num_obj.cpp
+++ Lab2_num_obj.cpp
@@ -13,4 +13,10 @@ int main() {
     char char_example = 'a';
     bool bool_example = true;
 
+  // sizeof returns the number of bytes (8 bits is equivalent to 1 byte))
+    cout << "int_example: " << sizeof(int_example) << endl;
+    cout << "float_example: " << sizeof(float_example) << endl;
+    cout << "double_example: " << sizeof(double_example) << endl;
+    cout << "char_example: " << sizeof(char_example) << endl;
+    cout << "bool_example: " << sizeof(bool_example) << endl;
 
