ae9c42d2ddf2d3ea302428398f6d731c15650abb
jonathanoa2004 <158521796+jonathanoa2004@users.noreply.github.com>
2024-02-19 23:16:23 -0800

    finished loop

--- Lab 3/Lab3_CF.cpp
+++ Lab 3/Lab3_CF.cpp
@@ -7,3 +7,8 @@ int main() {
   while (x < y) {
         std::cout << x << std::endl;
         x = x + 3;
+
+if (x % 2 == 0) {
+            std::cout << "x is even" << std::endl;
+        } else {
+            std::cout << "x is odd" << std::endl;
