fd049fd199402fa42067740c3881b05a20ec3126
jonathanoa2004 <158521796+jonathanoa2004@users.noreply.github.com>
2024-03-07 23:03:39 -0800

    Update Lab4_func.cpp

--- Lab4_func.cpp
+++ Lab4_func.cpp
@@ -2,52 +2,25 @@
 using namespace std;
 
 /** select
-* @brief output the statements based on func_type
-* func_type : output
-* "sum" : "The sum is "
-* "division" : "The quotient is "
-* "comparison" : "The first value being greater than the second value is "
-* All other phrases: "Invalid function type\n"
-* @param func_type : (string)
-* @return string
-*/
+ * @brief output the statements based on func_type
+ * func_type : output
+ * "sum" : "The sum is "
+ * "division" : "The quotient is "
+ * "comparison" : "The first value being greater than the second value is "
+ * All other phrases: "Invalid function type\n"
+ * @param func_type : (string)
+ * @return string
+ */
 string select(string func_type) {
-    string out;
     if (func_type == "sum") {
-        out = "The sum is ";
+        return "The sum is ";
     } else if (func_type == "division") {
-        out = "The quotient is ";
+        return "The quotient is ";
     } else if (func_type == "comparison") {
-        out = "The first value being greater than the second value is ";
+        return "The first value being greater than the second value is ";
     } else {
-        out = "Invalid function type\n";
+        return "Invalid function type\n";
     }
-    return out;
-}
-
-/** isGreaterThan
-* @brief returns whether x is greater than y
-*
-* @param x : (double)
-* @param y : (int)
-* @return true : x is greater than y
-* @return false : x is less than or equal to y
-*/
-bool isGreaterThan(double x, int y) {
-    return x > y;
-}
-
-
-/** sumOfThree
-* @brief returns the sum of x, y, and z
-*
-* @param x : (int)
-* @param y : (int)
-* @param z : (int)
-* @return int
-*/
-int sumOfThree(int x, int y, int z) {
-    return x + y + z;
 }
 
 /** sumOfThree
@@ -57,7 +30,7 @@ int sumOfThree(int x, int y, int z) {
  * @param y : (int)
  * @param z : (int)
  * @return int
-*/
+ */
 int sumOfThree(int x, int y, int z) {
     return x + y + z;
 }
@@ -68,47 +41,23 @@ int sumOfThree(int x, int y, int z) {
  * @param x : (double)
  * @param y : (double)
  * @return double
-*/
+ */
 double division(double x, double y) {
-    if (y != 0) {
-        return x / y;
-    } else {
+    if (y == 0) {
         cout << "Division by zero error" << endl;
         return 0; // or std::numeric_limits<double>::infinity() to indicate error
     }
+    return x / y;
 }
 
 /** isGreaterThan
  * @brief returns whether x is greater than y
  * 
- * @param x :(double)
+ * @param x : (double)
  * @param y : (int)
  * @return true : x is greater than y
  * @return false : x is less than or equal to y
-*/
+ */
 bool isGreaterThan(double x, int y) {
     return x > y;
-}
-
-/** select
- * @brief output the statements based on func_type
- * func_type : output
- * "sum"            : "The sum is "
- * "division"       : "The quotient is "
- * "comparison"     : "The first value being greater than the second value is "
- *  All other phrases: "Invalid function type"
- * 
- * @param func_type : (string)
- * @return string
-*/
-string select(const string& func_type) {
-    if (func_type == "sum") {
-        return "The sum is ";
-    } else if (func_type == "division") {
-        return "The quotient is ";
-    } else if (func_type == "comparison") {
-        return "The first value being greater than the second value is ";
-    } else {
-        return "Invalid function type\n";
-    }
 }
