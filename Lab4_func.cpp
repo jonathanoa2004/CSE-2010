64a25d001df995175048b3f608f871694934f782
jonathanoa2004 <158521796+jonathanoa2004@users.noreply.github.com>
2024-03-07 22:27:10 -0800

    Update Lab4_func.cpp

--- Lab4_func.cpp
+++ Lab4_func.cpp
@@ -9,7 +9,9 @@ using namespace std;
  * @param z : (int)
  * @return int
 */
-//write function here
+int sumOfThree(int x, int y, int z) {
+    return x + y + z;
+}
 
 /** division
  * @brief returns x / y
@@ -18,17 +20,26 @@ using namespace std;
  * @param y : (double)
  * @return double
 */
-// write function here
+double division(double x, double y) {
+    if (y != 0) {
+        return x / y;
+    } else {
+        cout << "Division by zero error" << endl;
+        return 0; // or std::numeric_limits<double>::infinity() to indicate error
+    }
+}
 
 /** isGreaterThan
  * @brief returns whether x is greater than y
  * 
  * @param x :(double)
- * @ param y : (int)
+ * @param y : (int)
  * @return true : x is greater than y
  * @return false : x is less than or equal to y
 */
-//write function here
+bool isGreaterThan(double x, int y) {
+    return x > y;
+}
 
 /** select
  * @brief output the statements based on func_type
@@ -36,47 +47,19 @@ using namespace std;
  * "sum"            : "The sum is "
  * "division"       : "The quotient is "
  * "comparison"     : "The first value being greater than the second value is "
- *  All other phrases: "Invalid function type/n"
+ *  All other phrases: "Invalid function type"
  * 
  * @param func_type : (string)
  * @return string
 */
-// write function here
-
-/** myPrint
- * @brief calls select() to print out the output with the correct prompt
- * 
- * @tparam T : (int, double, or string) output of sum, division, or isGreater Than
- * @param func_type : (string)
- * @param output
-*/
-template <typename T>
-void myPrint (string func_type, T output){
-    cout << select(func_type) << output << endl;
-}
-
-int main()
-{
-    // initialize 3 int parameters
-    int x1 = 2;
-    int x2 =3;
-    int x3 = 5;
-
-    //call sumOfThree
-    int sum_out = sumOfThree(x1, x2, x3);
-    // call myPrint
-    myPrint("sum", sum_out);
-
-    //initialize 2 double parameters
-    double dividend = 123345.1;
-    double divisor = 2345.6;
-
-    //call division
-    double div_out = division(dividend, divisor);
-    myPrint("division", div_out);
-
-    // compare div_out and sum_out
-    bool is_greater_out = isGreaterThan(div_out, sum_out);
-    myPrint("comparison", is_greater_out);
-    return 0
+string select(const string& func_type) {
+    if (func_type == "sum") {
+        return "The sum is ";
+    } else if (func_type == "division") {
+        return "The quotient is ";
+    } else if (func_type == "comparison") {
+        return "The first value being greater than the second value is ";
+    } else {
+        return "Invalid function type\n";
+    }
 }
