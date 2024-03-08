0d8cbd28ce78893a4591fdd7e3a045b6fe9fa3af
jonathanoa2004 <158521796+jonathanoa2004@users.noreply.github.com>
2024-03-07 22:14:14 -0800

    Update Lab4_func.cpp

---
+++ Lab4_func.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+
+/** sumOfThree
+ * @brief returns the sum of x, y, and z
+ * 
+ * @param x : (int)
+ * @param y : (int)
+ * @param z : (int)
+ * @return int
+*/
+//write function here
+
+/** division
+ * @brief returns x / y
+ * 
+ * @param x : (double)
+ * @param y : (double)
+ * @return double
+*/
+// write function here
+
+/** isGreaterThan
+ * @brief returns whether x is greater than y
+ * 
+ * @param x :(double)
+ * @ param y : (int)
+ * @return true : x is greater than y
+ * @return false : x is less than or equal to y
+*/
+//write function here
+
+/** select
+ * @brief output the statements based on func_type
+ * func_type : output
+ * "sum"            : "The sum is "
+ * "division"       : "The quotient is "
+ * "comparison"     : "The first value being greater than the second value is "
+ *  All other phrases: "Invalid function type/n"
+ * 
+ * @param func_type : (string)
+ * @return string
+*/
+// write function here
+
+/** myPrint
+ * @brief calls select() to print out the output with the correct prompt
+ * 
+ * @tparam T : (int, double, or string) output of sum, division, or isGreater Than
+ * @param func_type : (string)
+ * @param output
+*/
+template <typename T>
+void myPrint (string func_type, T output){
+    cout << select(func_type) << output << endl;
+}
+
+int main()
+{
+    // initialize 3 int parameters
+    int x1 = 2;
+    int x2 =3;
+    int x3 = 5;
+
+    //call sumOfThree
+    int sum_out = sumOfThree(x1, x2, x3);
+    // call myPrint
+    myPrint("sum", sum_out);
+
+    //initialize 2 double parameters
+    double dividend = 123345.1;
+    double divisor = 2345.6;
+
+    //call division
+    double div_out = division(dividend, divisor);
+    myPrint("division", div_out);
+
+    // compare div_out and sum_out
+    bool is_greater_out = isGreaterThan(div_out, sum_out);
+    myPrint("comparison", is_greater_out);
+    return 0
+}
